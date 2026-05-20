887983684b83a01ebbce43313aa8869a22b6c57d
Cahal Agarwalla <cahalagarwalla333@gmail.com>
2026-05-20 13:48:26 +0530

    ip changed

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
 // Replace this with your EC2 instance IP address after running 'terraform apply'
 // For example: export const EC2_IP = "13.234.11.22"
-export const EC2_IP = "13.204.67.38";
+export const EC2_IP = "3.7.69.48";
